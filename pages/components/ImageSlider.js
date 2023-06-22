43ee1f37700af8c6f742ea3e9c6c844f2ce3696c
NirajVadher07 <nirajvvadher0711@gmail.com>
2023-06-22 13:10:44 +0530

    About page added and ImageSlider chages

--- pages/components/ImageSlider.js
+++ pages/components/ImageSlider.js
@@ -3,24 +3,6 @@ import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
 import { RxDotFilled } from 'react-icons/rx';
 
 const ImageSlider = ({ gallery }) => {
-    // const slides = [
-    //     {
-    //         url: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80',
-    //     },
-    //     {
-    //         url: 'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
-    //     },
-    //     {
-    //         url: 'https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80',
-    //     },
-
-    //     {
-    //         url: 'https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80',
-    //     },
-    //     {
-    //         url: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80',
-    //     },
-    // ];
     const slides = []
 
     const [currentIndex, setCurrentIndex] = useState(0);
@@ -72,7 +54,7 @@ const ImageSlider = ({ gallery }) => {
                                 <div
                                     key={slideIndex}
                                     onClick={() => goToSlide(slideIndex)}
-                                    className='text-2xl cursor-pointer'
+                                    className={`text-2xl cursor-pointer ${slideIndex==currentIndex ? ("text-[#590DE1] mx-5") : ("")}`}
                                 >
                                     <RxDotFilled />
                                 </div>
